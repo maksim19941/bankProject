1b1c1dff49e3000d90d5dcee7d10a04daa4b5359
sergey <mysmurov@gmail.com>
2024-03-05 09:04:19 +0300

    profile start

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfileApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ProfileApplication.class, args);
+
     }
 }
